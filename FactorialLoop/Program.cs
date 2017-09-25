8d9e571ada9f2108652285616065da62875bc697
Bob <bobevans547@gmail.com>
2017-09-25 16:03:51 -0400

    FactorialLoop

--- FactorialLoop/Program.cs
+++ FactorialLoop/Program.cs
@@ -8,8 +8,30 @@ namespace FactorialLoop
 {
     class Program
     {
-        static void Main(string[] args)
+        int Factorial(int nbr)
         {
+            var factorial = 1;
+            for (var idx = 2; idx <= nbr; idx++)
+            {
+                factorial *= idx;
+            }
+            return factorial;
+        }
+        bool IsDivisibleByThree(int nbr) {
+            return (nbr % 3 == 0);
+        }
+        void Run() {
+            for (var nbr=2; nbr<= 10; nbr++) {
+                Console.WriteLine($"The Factorial of {nbr} is {Factorial(nbr)}");
+            }
+            
+        }
+        static void Main(string[] args){
+            new Program().Run();
         }
     }
+
+
+
+
 }
